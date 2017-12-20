6a9325ad3ed031af452186ed7c4b614835804283
Youssef Moumni <youssef.moumni1013@gmail.com>
2017-12-20 16:26:23 +0000

    Change-Id: I0000000000000000000000000000000000000000 jj
    Change-Id: If40694008a1cd2dedaeb3baf8a8d582b76982882

--- maintenance/src/main/java/ma/emsi/maintenance/controllers/MachineContoller.java
+++ maintenance/src/main/java/ma/emsi/maintenance/controllers/MachineContoller.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,7 @@ import ma.emsi.maintenance.services.MachineService;
 
 @Controller
 @RequestMapping("machine") 
-
+@CrossOrigin(origins = "http://localhost:8888")
 public class MachineContoller {
 
 	@Autowired
@@ -24,6 +26,12 @@ public class MachineContoller {
 	@ResponseBody
 	public List<Machine> allmachines() {
 		return machineService.getallMachines();	
+	}
+	
+	@GetMapping("/liste")
+	public String main(Model m) {
+		m.addAttribute("machines", machineService.getallMachines());
+		return "CA";
 	}
 	
 	@RequestMapping("/atelier/{idatelier}")
