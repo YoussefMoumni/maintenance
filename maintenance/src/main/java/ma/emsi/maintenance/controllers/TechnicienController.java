ab37ea40d04ca6eda4be4bf56105e6c511089dce
Youssef Moumni <youssef.moumni1013@gmail.com>
2017-12-22 11:56:46 +0000

    Change-Id: I0000000000000000000000000000000000000000 ll
    Change-Id: Ib9fd2d2cbb0324fc3128ec8e573f40ec8332e08b

--- maintenance/src/main/java/ma/emsi/maintenance/controllers/TechnicienController.java
+++ maintenance/src/main/java/ma/emsi/maintenance/controllers/TechnicienController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,8 @@ import ma.emsi.maintenance.services.TechnicienService;
 
 @Controller 
 @RequestMapping("T") 
+@CrossOrigin(origins = "http://localhost:8888")
+
 public class TechnicienController {
 	@Autowired
 	TechnicienService technicienService;
